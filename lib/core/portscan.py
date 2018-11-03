5e4f6bc0339c16d0917c39a55dbe38204404911c
wh1t3P1g <wh1t3p1g@gmail.com>
2018-11-03 18:06:06 +0800

    alter port scan

--- lib/core/portscan.py
+++ lib/core/portscan.py
@@ -14,7 +14,7 @@ class PortScanner(object):
 
     def __init__(self, host, ports=None, loop=None):
         self.host = host
-        self.ports = ports if ports else "80,443,8080,8081"
+        self.ports = ports if ports else "80,443,8080,8081,7001,8888,8880"
         self.ret = []
         self.loop = loop
 
@@ -36,16 +36,25 @@ class PortScanner(object):
                 self.ret.append(self.host+":"+port)
                 logger.info("Found "+self.host+":"+port)
 
+def assign(scanners,loop):
+    tasks = []  # todo 控制一下速度 太快会崩
+    for scanner in scanners:
+        tasks.append(asyncio.ensure_future(scanner.run()))
+    for task in tasks:
+        loop.run_until_complete(task)
+
 def port_scan(subdomains, ports=None):
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
     scanners = [PortScanner(subdomain,ports,loop) for subdomain in subdomains]
-    tasks = []
-    for scanner in scanners:
-        tasks.append(asyncio.ensure_future(scanner.run()))
-    for task in tasks:
-        loop.run_until_complete(task)
+    step = 30
+    start = step
+    tmp = scanners[0:step]
+    while tmp:
+        assign(tmp,loop)
+        tmp = scanners[start:start+step]
+        start += step
     loop.close()
     ret = []
     for scanner in scanners:
