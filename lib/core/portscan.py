f6ef60c6ea9118d286161953b48d73fbcf55bf78
wh1t3P1g <wh1t3p1g@gmail.com>
2018-11-03 18:37:02 +0800

    alter port scan

--- lib/core/portscan.py
+++ lib/core/portscan.py
@@ -14,9 +14,25 @@ class PortScanner(object):
 
     def __init__(self, host, ports=None, loop=None):
         self.host = host
-        self.ports = ports if ports else "80,443,8080,8081,7001,8888,8880"
+        self.make_ports()
+        # self.ports = ports if ports else "80,443,8080,8081,7001,8888,8880,3128,9080,9090,9200,9300"
         self.ret = []
         self.loop = loop
+
+    def make_ports(self):
+        # 80
+        # 80-90
+        # 8000-9090
+        # 7001,7002 weblogic
+        # 9200,9300 elasticsearch
+        # 6379 redis未授权
+        # 5984 CouchDB http://xxx:5984/_utils/
+        # 11211 memcache未授权访问
+        # 27017,27018 Mongodb未授权访问
+        ports = [80,7001,7002,9200,9300,6379,5984,11211,27017,27018]
+        ports.extend([i for i in range(81,91)])
+        ports.extend([i for i in range(8000,9091)])
+        self.ports = ports
 
     @staticmethod
     async def socket(host, port, loop):
@@ -36,39 +52,12 @@ class PortScanner(object):
                 self.ret.append(self.host+":"+port)
                 logger.info("Found "+self.host+":"+port)
 
-# def assign(scanners,loop):
-#     tasks = []  # todo 控制一下速度 太快会崩
-#     for scanner in scanners:
-#         tasks.append(asyncio.ensure_future(scanner.run()))
-#     for task in tasks:
-#         loop.run_until_complete(task)
-#
-# def port_scan(subdomains, ports=None):
-#     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-#     loop = asyncio.get_event_loop()
-#     loop.set_debug(True)
-#     scanners = [PortScanner(subdomain,ports,loop) for subdomain in subdomains]
-#     step = 30
-#     start = step
-#     tmp = scanners[0:step]
-#     while tmp:
-#         assign(tmp,loop)
-#         tmp = scanners[start:start+step]
-#         start += step
-#     loop.close()
-#     ret = []
-#     for scanner in scanners:
-#         if len(scanner.ret) > 0:
-#             ret.extend(scanner.ret)
-#     return ret
-
-
 def port_scan(subdomains, ports=None):
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
     scanners = [PortScanner(subdomain,ports,loop) for subdomain in subdomains]
-    tasks = []  # todo 控制一下速度 太快会崩
+    tasks = []
     for scanner in scanners:
         tasks.append(asyncio.ensure_future(scanner.run()))
     for task in tasks:
